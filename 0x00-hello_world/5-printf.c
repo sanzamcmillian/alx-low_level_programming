e56c2620b0355afc9553b2f5276baaad9e5ce107
sanzamcmillian <g18s658@ru.ac.za>
2023-06-15 11:32:37 -0700

    mod

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - entry point
+ * main - Entry point
  *
- * return:always 0(succsess)
+ * Return:Always 0(Succsess)
  */
 int main(void)
 {
-	printf("with proper grammar,but the outcome is a piece of art,\n");
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
