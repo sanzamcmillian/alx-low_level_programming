1c664925c3cfc246c24ab4fed05f166712c955a2
sanzamcmillian <g18s658@ru.ac.za>
2023-06-18 07:24:30 -0700

    positives and negatives

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,14 +19,14 @@ int main(void)
 	}
         else
 	{
-	       if (n == 0)
-	       {
-		       printf("%d is zero\n", n);
-	       }
-	       else
-	       {
-		       printf("%d is negative\n", n);
-	       }
+	       	if (n == 0)
+	        {
+		        printf("%d is zero\n", n);
+	        }
+	        else
+	        {
+		        printf("%d is negative\n", n);
+	        }
 	}
 	return (0);
 }
