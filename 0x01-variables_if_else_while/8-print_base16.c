c3e9f89c32d0ea1414896dba54a466b84ac74897
sanzamcmillian <g18s658@ru.ac.za>
2023-06-18 10:46:38 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	int n;
+	char ch;
+	for (n = 0; n < 10; n++)
+	{
+		putchar('0' + n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
