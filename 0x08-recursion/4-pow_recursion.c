388844e6c1880cd3b2d7240fd54c2e4e0c5a572a
sanzamcmillian <g18s2658@ru.ac.za>
2023-07-05 19:28:18 +0200

    pweer

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,22 +4,22 @@
  *_pow_recursion - returns x raised to the power of y
  *
  *@x: base
- *y: power
- *Return: -1(y lower than zero) and results otherwise
+ *@y: power
+ *Return: -1(y lower than zero), 1(if y is zero) and results otherwise
  */
 
 int _pow_recursion(int x, int y)
 {
-        if (y < 0)
-        {
-                return (-1);
-        }
+	if (y < 0)
+	{
+		return (-1);
+	}
 	else if (y == 0)
 	{
 		return (1);
 	}
-        else
-        {
-                return (x *_pow_recursion(x, y - 1));
-        }
+	else
+	{
+		return (x *_pow_recursion(x, y - 1));
+	}
 }
