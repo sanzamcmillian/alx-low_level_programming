0c5f70402a1aa70591e8e7893370276a02cad385
sanzamcmillian <g18s2658@ru.ac.za>
2023-07-05 16:33:51 +0200

    new

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_puts_recursion -  prints a string, followed by a new line
+ *
+ *@s: takes in a series of characters
+ *Return: Nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s =="\0")
+	{
+		return;
+	}
+
+	_putchar(s);
+	s++;
+	_puts_recursion(s);
+	_putchar("\n");
+	
+}
