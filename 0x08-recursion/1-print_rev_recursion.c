0588d50851fa0b129df470c3307574d889146633
sanzamcmillian <g18s2658@ru.ac.za>
2023-07-05 17:16:08 +0200

    new

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - prints a string in reverse
+ *
+ *@s: string to  be reversed
+ *Return: Nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s);
+		s--;
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
