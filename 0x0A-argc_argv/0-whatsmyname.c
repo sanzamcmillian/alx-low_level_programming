2502985a66ebf292350b202ffe0ff294948fd791
sanzamcmillian <g18s2658@ru.ac.za>
2023-07-07 21:12:23 +0200

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *whatsmyname - prints its name
+ * 
+ *@argc: number of commands
+ *@argv: strings in the program
+ *Return: Always 0
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n",argv[argc - 1]);
+	return (0);
+
+}
