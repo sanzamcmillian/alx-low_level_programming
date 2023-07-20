456e74a161f1d6bd8265e4563fa6dacc1d9e4975
sanzamcmillian <g18s2658@ru.ac.za>
2023-07-20 14:00:44 +0200

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,12 +4,10 @@
  *print_name - prints name using other functions
  *@name: name of the person
  *@*f: function as a parameter
- *
  *Return: Nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	(*f)(name);
-
 }
