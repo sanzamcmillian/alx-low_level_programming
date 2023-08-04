1418b999875a1eb40e7ed6e031abe2126d172609
sanzamcmillian <g18s2658@ru.ac.za>
2023-08-04 21:34:15 +0200

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,7 +2,7 @@
 
 /**
  *set_bit - sets the value of a bit to 1 at a given index
- *@n: pointer to the number to change 
+ *@n: pointer to the number to change
  *@index: index of the bit to set to 1
  *
  *Return: 1 for success, -1 for failure
